e2f0b715d12d2a73b13a09fd9100c3ef509a14f7
Ruslan <feskovrv@yandex.ru>
2025-09-11 20:46:19 +0300

    рефакторинг

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,11 +6,11 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public interface FilmStorage {
-    Film addFilm(Film film);
-    Film updateFilm(Film film);
-    Collection<Film> findAllFilms();
-    Film findFilmById(int id);
-    Film addLikeToFilm(int id, int userId);
-    Film removeLikeFromFilm(int id, int userId);
-    ArrayList<Film> getTopFilms(int count);
+    public Film addFilm(Film film);
+    public Film updateFilm(Film film);
+    public Collection<Film> findAllFilms();
+    public Film findFilmById(int id);
+    public Film addLikeToFilm(int id, int userId);
+    public Film removeLikeFromFilm(int id, int userId);
+    public ArrayList<Film> getTopFilms(int count);
 }
