5802c1da1d4e2767dd24b1b06c316d83b1780bd7
Ruslan <feskovrv@yandex.ru>
2025-09-11 21:23:44 +0300

    рефакторинг

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -8,11 +8,18 @@ import java.util.HashSet;
 
 public interface UserStorage {
     User addUser(User user);
+
     User updateUser(User user);
+
     Collection<User> findAllUsers();
+
     User findUserById(int id);
+
     User addFriend(int id, int friendId);
+
     User deleteFriend(int id, int friendId);
+
     ArrayList<User> getAllUserFriends(int id);
+
     HashSet<User> getAllCommonFriends(int id, int otherId);
 }
